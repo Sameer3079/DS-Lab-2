4e4b2422700a98fcf6c3606faedaee672969a4d9
Sameer <smrbasil4@gmail.com>
2018-03-21 15:33:58 +0530

    Added the methods to the interface

--- MathService.java
+++ MathService.java
@@ -1,9 +1,11 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
- 
+
 public interface MathService extends Remote{
     public int add(int a, int b) throws RemoteException;
     public int subtract(int a, int b) throws RemoteException;
     public int multiply(int a, int b) throws RemoteException;
     public int divide(int a, int b) throws RemoteException;
+    public double square_root(double a) throws RemoteException;
+    public double square(double a) throws RemoteException;
 }
